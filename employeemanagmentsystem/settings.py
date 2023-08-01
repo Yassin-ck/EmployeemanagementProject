2c57f44cb89c5fb59f9ac8d846b7eb8d41e428c2
Yassin-ck <mohammedyassinck@gmail.com>
2023-08-01 16:07:51 +0530

    dot

--- employeemanagmentsystem/settings.py
+++ employeemanagmentsystem/settings.py
@@ -170,7 +170,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
