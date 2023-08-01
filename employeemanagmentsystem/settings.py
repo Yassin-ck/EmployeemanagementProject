6e3827e75be5609701b4ecb3d44c5e2cf7ef9926
Yassin-ck <mohammedyassinck@gmail.com>
2023-08-01 18:13:38 +0530

    host

--- employeemanagmentsystem/settings.py
+++ employeemanagmentsystem/settings.py
@@ -210,11 +210,5 @@ AWS_S3_REGION_NAME = 'eu-north-1'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL =  None
 AWS_S3_VERITY = True
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
-    },
-    "default": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-    },
-}
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
